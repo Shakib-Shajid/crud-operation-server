7bc124739b6c617086ce600c939a5084b08a0ab7
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-07-01 02:08:11 +0600

    post and get query added

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
 
 // middleware
 app.use(cors());
 app.use(express.json())
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvn5rcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,6 +26,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    // .....................................................
+
+    const userCollection = client.db("usersDB").collection("users");
+
+
+    // get data from server
+    app.get('/users', async (req,res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // post = send data in server to store
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+    // .....................................................
+    
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,11 +62,11 @@ async function run() {
 run().catch(console.dir);
 
 
-
 app.get('/', (req, res) => {
-    res.send('users management server is running');
+  res.send('users management server is running');
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on PORT: ${port}`);
+  console.log(`Server is running on PORT: ${port}`);
 })
+
